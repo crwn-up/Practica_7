7d9298d4899b713e5d24f3bb81699db12152058d
crwn-up <luidran1@gmail.com>
2025-03-27 14:08:25 -0600

    Create Ejercicio_2.c

---
+++ Ejercicio_2.c
@@ -0,0 +1,17 @@
+// Luis Crown
+//Ejercicio 2
+# include <stdio.h> 
+void main ()
+{
+       int número;
+        printf("ingresa un número");
+        scanf("%d",&número);
+        if (número %2 ==0)
+    {
+        printf ("el número ingresado es par.\n");
+    }
+    else
+    {
+        printf ("ingresaste un número impar\n");
+    }
+}
