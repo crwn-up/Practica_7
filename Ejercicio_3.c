06a5d4baec7023ee3e3c2730a82fd347d7486334
crwn-up <luidran1@gmail.com>
2025-03-27 14:54:26 -0600

    Update Ejercicio_3.c

--- Ejercicio_3.c
+++ Ejercicio_3.c
@@ -6,7 +6,7 @@
      int opcion;
      double num1, num2, resultado;
      
-     printf("Calculadora de Luis Crown,operaciones disponibles");
+     printf("Calculadora de Luis Crown,operaciones disponibles:\n");
      printf("1. suma\n");
      printf("2. resta\n");
      printf("3. multiplicación\n");
@@ -15,26 +15,27 @@
      printf("6. Exponente\n");
      printf("Seleccione una opción (1-6)");
      scanf("%d", &opcion);
+     
      switch (opcion){
          case 1: // suma
          printf("ingrese dos numeros");
          scanf("%lf %lf", &num1, &num2);
          resultado = num1 + num2;
-         printf("resultado: %.2lf\n");
+         printf("resultado: %.2lf\n", resultado);
          break;
          
          case 2: // resta
-         printf("ingrese dos numeros");
+         printf("ingrese dos numeros:");
          scanf("%lf %lf", &num1, &num2);
          resultado = num1 - num2;
-         printf("Resultado: %.2lf\n");
+         printf("Resultado: %.2lf\n",resultado);
          break;
         
         case 3: // multiplicación
          printf("ingrese dos numeros");
          scanf("%lf %lf", &num1, &num2);
          resultado = num1 * num2;
-         printf("Resultado: %.2lf\n");
+         printf("Resultado: %.2lf\n", resultado);
          break;
          
          case 4: // división
@@ -42,27 +43,27 @@
          scanf("%lf %lf", &num1, &num2);
          if (num2 !=0){
          resultado = num1 / num2;
-         printf("Resultado: %.2lf");
+         printf("Resultado: %.2lf\n", resultado);
          } else {
              printf("error: no se puede dividir entre 0");
          }
          break;
          
-         case 5: // Ra´íz cuadrada
-         printf("ingrese un número");
+         case 5: // Raíz cuadrada
+         printf("ingrese un número:");
          scanf("%lf", &num1);
          if (num1 >=0){
          resultado = sqrt(num1);
-         printf("Resultado: %.2lf");
+         printf("Resultado: %.2lf\n", resultado);
          } else {
-             printf("error: no se puede calcular la raíz de 0");
+             printf("error: no se puede calcular");
          }
          break;
          
          case 6: // Exponente
          printf("ingrese la base y el exponente");
-         scanf("%lf", &num1, num2);
+         scanf("%lf %lf", &num1, &num2);
          resultado = pow(num1, num2);
-         printf("Resultado: %.2lf");
-     
-    
+         printf("Resultado: %.2lf\n", resultado);
+     }
+}
