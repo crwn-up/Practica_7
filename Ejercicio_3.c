c6724287ddef6846fea8a047333ff2ea99114ce5
crwn-up <luidran1@gmail.com>
2025-03-27 14:35:31 -0600

    Create Ejercicio_3.c

---
+++ Ejercicio_3.c
@@ -0,0 +1,68 @@
+// Luis Crown
+//Ejercicio 3
+#include <stdio.h>
+#include <math.h>
+ int main() {
+     int opcion;
+     double num1, num2, resultado;
+     
+     printf("Calculadora de Luis Crown,operaciones disponibles");
+     printf("1. suma\n");
+     printf("2. resta\n");
+     printf("3. multiplicación\n");
+     printf("4. División\n");
+     printf("5. Raíz cuadrada\n");
+     printf("6. Exponente\n");
+     printf("Seleccione una opción (1-6)");
+     scanf("%d", &opcion);
+     switch (opcion){
+         case 1: // suma
+         printf("ingrese dos numeros");
+         scanf("%lf %lf", &num1, &num2);
+         resultado = num1 + num2;
+         printf("resultado: %.2lf\n");
+         break;
+         
+         case 2: // resta
+         printf("ingrese dos numeros");
+         scanf("%lf %lf", &num1, &num2);
+         resultado = num1 - num2;
+         printf("Resultado: %.2lf\n");
+         break;
+        
+        case 3: // multiplicación
+         printf("ingrese dos numeros");
+         scanf("%lf %lf", &num1, &num2);
+         resultado = num1 * num2;
+         printf("Resultado: %.2lf\n");
+         break;
+         
+         case 4: // división
+         printf("ingrese dos numeros");
+         scanf("%lf %lf", &num1, &num2);
+         if (num2 !=0){
+         resultado = num1 / num2;
+         printf("Resultado: %.2lf");
+         } else {
+             printf("error: no se puede dividir entre 0");
+         }
+         break;
+         
+         case 5: // Ra´íz cuadrada
+         printf("ingrese un número");
+         scanf("%lf", &num1);
+         if (num1 >=0){
+         resultado = sqrt(num1);
+         printf("Resultado: %.2lf");
+         } else {
+             printf("error: no se puede calcular la raíz de 0");
+         }
+         break;
+         
+         case 6: // Exponente
+         printf("ingrese la base y el exponente");
+         scanf("%lf", &num1, num2);
+         resultado = pow(num1, num2);
+         printf("Resultado: %.2lf");
+     
+    
