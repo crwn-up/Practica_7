f92d71d57790dc1d8aaec9cc1d99b9b10cdb8c2c
crwn-up <luidran1@gmail.com>
2025-03-27 13:58:47 -0600

    Update Ejercicio_1.c

--- Ejercicio_1.c
+++ Ejercicio_1.c
@@ -1,1 +1,20 @@
-
+// Luis Crown
+//Ejercicio 1
+# include <stdio.h> 
+void main ()
+{
+       int a, b;
+        printf("ingresa el valor de a");
+        scanf("%d",&a);
+        
+        printf("ingresa el valor de b");
+        scanf ("%d", &b);
+        if (a>b)
+    {
+        printf ("a es mayor que b \n");
+    }
+    else
+    {
+        printf ("b es mayor o igual a \n");
+    }
+}
